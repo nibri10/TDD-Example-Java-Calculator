3fb8c452f8c18a77dfdc4201e39816f9cc947229
Nicolas Oliveira <nibri.kcond2011@gmail.com>
2019-02-06 22:26:28 -0200

    Refactoring CalculadoradeSalario.java

--- src/main/java/Model/CalculadoradeSalario.java
+++ src/main/java/Model/CalculadoradeSalario.java
@@ -5,20 +5,28 @@ public class CalculadoradeSalario {
     public double calcularSalario(Funcionario funcionario){
 
         if(funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)){
-            if(funcionario.getSalario() > 3000) {
-                return funcionario.getSalario() * 0.8;
-            }
-            return funcionario.getSalario() * 0.9;
+          return dez_Ou_Vinte_Por_Cento_De_Desconto(funcionario);
         }
        else if(funcionario.getCargo().equals(Cargo.DBA) || funcionario.getCargo().equals(Cargo.TESTADOR)) {
-           if(funcionario.getSalario() < 2500){
-               return  funcionario.getSalario() * 0.85;
-           }
-           return funcionario.getSalario() * 0.75;
+         return Quinze_Ou_VinteeCinco_Porcento_de_Desconto(funcionario);
 
         }
         throw  new RuntimeException("Funcionario invalido");
 
+    }
+
+    private double Quinze_Ou_VinteeCinco_Porcento_de_Desconto(Funcionario funcionario){
+        if(funcionario.getSalario() < 2500){
+            return  funcionario.getSalario() * 0.85;
+        }
+        return funcionario.getSalario() * 0.75;
+    }
+
+    private double dez_Ou_Vinte_Por_Cento_De_Desconto(Funcionario funcionario){
+        if(funcionario.getSalario() > 3000) {
+            return funcionario.getSalario() * 0.8;
+        }
+        return funcionario.getSalario() * 0.9;
     }
 
 
